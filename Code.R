8541c66743d958b33408dcf90262424a158e7777
RT <52295738+RT13by@users.noreply.github.com>
2021-02-15 12:40:38 +0300

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,13 +1,12 @@
-# Ââîä èñõîäíûõ äàííûõ (òåìïà ïðèðîñòà ðåàëüíîãî ÂÂÏ (GDPr), PIT PD â ðàçðåçå ãðóïï êðåäèòíîãî ðåéòèíãà (Õ1…Õ9)) 
-# â ïåðåìåííóþ (input) ïîñðåäñòâîì R ,ïðèâåäåíèÿ èõ â ïðèãîäíûé
-# äëÿ îáðàáîòêè âèä è ïðîâåðêè èõ êîððåêòíîñòè
+#  data input  (growth rate of real GDP (GDPr), PIT PD in the groups of credit rating (X1 ... X9))
+#  в into a variable (input) to convert  it into a form suitable for processing and checking  correctness 
 # I:
 input <-read.csv("input_data.csv")
 library(dplyr)
 dplyr::glimpse(input)
 
-# Äëÿ âèçóàëüíîé îöåíêè öåëåñîîáðàçíî ïîñòðîåíèå ãðàôèêîâ ïåðåìåííûõ 
-#(óðîâíÿ ðåàëüíîãî ÂÂÏ è PIT PD íà ïðèìåðå 9 ãðóïïû êðåäèòíîãî ðåéòèíãà)
+# To assess visually, it is advisable to plot graphs of variables
+# (growth rate of real GDP (GDPr), PIT PD for 9 group of credit rating as an example)
 # II:
 dates<-seq(as.Date("2016/1/1"), as.Date("2018/10/1"), by = "quarter")
 plot(dates, input$X9, type="o", main="DR 9 group",
@@ -15,7 +14,7 @@ ylab="Default rate",xlab="Time", col="blue")
 plot(dates, input$GDPr, type="o", main="GDP growth rate",
 ylab="Percent",xlab="Time", col="blue")
 
-# Ïîñòðîåíèå ãèñòîãðàìì âðåìåííûõ ðÿäîâ óðîâíÿ ðåàëüíîãî  ÂÂÏ è PIT PD íà ïðèìåðå 9 ãðóïïû êðåäèòíîãî ðåéòèíãà
+# Construction of histograms of time series (growth rate of real GDP (GDPr), PIT PD for 9 group of credit rating as an example) 
 # III:
 library(ggplot2)
 hist.adverts <-ggplot(input, aes(input$X9))+
@@ -33,29 +32,29 @@ hist.adverts + stat_function(fun = dnorm, args = list(mean = mean(input$GDPr, na
 sd = sd(input$GDPr, na.rm = TRUE)), color  = 'red')
 
 
-# KPSS-òåñò
+# KPSS-testing
 # IV:
 library(tseries)
 kpss.test(input$X9, null = "Trend", lshort = TRUE)
 kpss.test(input$GDPr, null = "Trend", lshort = TRUE)
 
-# Ðàñ÷åò ëèíåéíîé ðåãðåññèè îñóùåñòâëåí íà ïðèìåðå 9 ãðóïïû êðåäèòíîãî ðåéòèíãà
+# The construction of linear regression model (growth rate of real GDP (GDPr), PIT PD for 9 group of credit rating as an example) 
 # V:
 fit <-lm(X9~GDPr, data=input)
 summary(fit)
 confint(fit)
 
-# Îöåíêà äîïóùåíèé ìåòîäà íàèìåíüøèõ êâàäðàòîâ 
+# Least Squares Assumptions check
 # VI:
 library(gvlma)
 summary(gvlma(fit))
 
-# Òåñò Äàðáèíà-Óîòñîíà
+# Durbin-Watson test
 # VII:
 library(car) 
 durbinWatsonTest(fit)
 
-# Òåñò Áðîéøà-Ãîäôðè
+# Breusch-Godfrey test
 # VIII:
 library(lmtest)
 bg3 <-bgtest(fit, order = 3)
@@ -63,7 +62,7 @@ bg3
 bg4 <-bgtest(fit, order = 4)
 bg4
 
-# Ïîñòðîåíèå ãðàôèêîâ ÀÊÔ è ÏÀÊÔ
+# ACF and PACF calculation and testing
 # IX:
 res <- residuals(fit)
 Res
